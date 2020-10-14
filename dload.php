c0abe5d02849b42f79a172953b03d38a809cc4ef
BsNoSi <nosi@nosi.de>
2020-10-14 12:54:06 +0200

    Update 0.5.0

--- dload.php
+++ dload.php
@@ -6,40 +6,38 @@
 // requires YELLOW 0.8.4 or higher
 
 class YellowDLoad {
-    const VERSION = "0.0.5";
-    const TYPE = "feature";
-    public $yellow;         //access to API
+  const VERSION = "0.5.0";
+  public $yellow;
 	   
-    // Handle initialisation
-    public function onLoad($yellow) {
-        $this->yellow = $yellow;
-    }
+  public function onLoad($yellow) {
+    $this->yellow = $yellow;
+  }
 	 
-    // Handle page content of shortcut
-    public function onParseContentShortcut($page, $name, $text, $type) {
-        $output = null;
-        if ($name=="dload") {
-            list($data, $area, $note) = $this->yellow->toolbox->getTextArgs($text);
-		 if ($data<>"") {
-			       $area = ($area <>"") ? ' id="'.$area.'" ' : "";
-                         $output = '<div ' .$area. 'w3-include-html="'.$data.'">'. $note .'</div>'; 				
-		}
-		 else {
-			 $output = null;
-		 }
-        }
-        return $output;
+  public function onParseContentShortcut($page, $name, $text, $type) {
+    $output = null;
+    if ($name=="dload") {
+      list($data, $area, $note) = $this->yellow->toolbox->getTextArgs($text);
+      if ($data<>"") {
+       $output = '<div class="' . $area .'"><div id="replacespace" src="'. $data . '">' . $note .'</div></div>';
+      }
+	else {
+	  $output = null;
 	}
-	// Handle page extra data
-	public function onParsePageExtra($page, $name) {
-        $output = null;
-//      if ($name == "footer" && $page->isExisting("dload"))  {
-          if ($name == "footer" && (strpos($this->yellow->page->getContent($rawFormat = true, $sizeMax = 0),"dload")))  {
-		$extensionLocation = $this->yellow->system->get("serverBase").$this->yellow->system->get("extensionLocation");
-   		$output = "<script src=\"{$extensionLocation}dload.js\"></script>\n";
-		$output .= "<script>includeHTML();</script>\n";
-        }
-	  return $output;
-	}
+    }
+    return $output;
+  }
+	
+  public function onParsePageExtra($page, $name) {
+    $output = null;
+    
+    if ($name == "footer" && $page->isExisting("dload"))  {
+      $output = "<script>\n";
+	$output .= "filetoopen = document.getElementById('replacespace').getAttribute('src');\n";
+	$output .= "var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');xhr.open('get', filetoopen, true);xhr.onreadystatechange = function() {if (xhr.readyState == 4 && xhr.status == 200) {  document.getElementById('replacespace').innerHTML = xhr.responseText; } }\n";
+	$output .= "xhr.send();\n";
+	$output .= "</script>\n";
+    }
+    return $output;
+  }
 }
 ?>
